44273c1efb416f26b09114cc2916fa92c57e9431
ruthpatino <ruth.patino@utp.ac.pa>
2020-07-26 19:50:35 -0500

    commit final

--- app.js
+++ app.js
@@ -1,7 +1,48 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser');
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
- 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+const ESTUDIANTES = [
+    {
+        nombre: "Ruth Patiño",
+        edad: 23,
+    },
+    {
+        nombre: "Yulianis Benis",
+        edad: 18,
+    }
+
+];
+
+app.use(bodyparser.json());
+
+//CONTROLADORES
+app.get('/api/estudiantes/', (req, res) =>{
+    res.json({
+        cantidad: ESTUDIANTES.length,
+        estudiantes: ESTUDIANTES
+    });
+});
+
+app.get('/api/estudiantes/:id',(req, res) => {
+    res.json(ESTUDIANTES[req.params.id]);
+});
+
+app.post('/api/estudiantes/',(req, res) => {
+   ESTUDIANTES.push(req.body);
+   res.json(req.body);
+});
+
+app.put('/api/estudiantes/:id',(req, res) => {
+    ESTUDIANTES.push(req.body);
+    res.json(Estudiantes[req.params.id]);
+});
+  
+app.delete('/api/estudiantes/:id',(req, res) => {
+    res.json(Estudiantes.splice(req.params.id));
+    res.send(Estudiantes)
+});
+
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
